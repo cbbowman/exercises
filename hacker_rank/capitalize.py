8f0df9ec49cd39ee6499042955f509c69dd481b9
Charlie Bowman <c.b.bowman@outlook.com>
2023-03-09 16:01:21 -0500

    hackerrank exercise

--- hacker_rank/capitalize.py
+++ hacker_rank/capitalize.py
@@ -4,7 +4,7 @@ def capitalize(text: str) -> str:
     if len_s == 0:
         return text
 
-    words = text.split()
+    words = text.split(" ")
 
     for i, w in enumerate(words):
         words[i] = w.capitalize()
@@ -16,5 +16,10 @@ def capitalize(text: str) -> str:
 
 input = "charlie bowman"
 output = "Charlie Bowman"
+
+assert capitalize(input) == output
+
+input = "q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M"
+output = "Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M"
 
 assert capitalize(input) == output
